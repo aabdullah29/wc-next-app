2024eca5ccb3d01c601617759ddd1920a457e07c
abdullah <abdullah29dev@gmail.com>
2023-11-23 13:20:28 +0500

    add solana button in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,12 @@ export default function Home() {
   const router = useRouter();
   // set selecter token's data
   const handleOptionChange = (event: ChangeEvent<HTMLInputElement>) => {
-    router.push(`/selectToken/${event.target.value}`);
+    if(event.target.value === "solana"){
+      router.push(`/solanaTransfer`);
+
+    }else{
+      router.push(`/selectToken/${event.target.value}`);
+    }
   };
 
   return (
@@ -28,6 +33,12 @@ export default function Home() {
           <RadioButton
             label="Mumbai"
             value="polygon_mumbai"
+            checked={false}
+            onChange={handleOptionChange}
+          />
+          <RadioButton
+            label="Solana"
+            value="solana"
             checked={false}
             onChange={handleOptionChange}
           />
