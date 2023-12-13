92b33c34dfd7b2e1790f55a61d5d139373f66ed0
abdullah <abdullah29dev@gmail.com>
2023-12-13 21:17:38 +0500

    useWaitTransaction for sendERC20

--- src/app/transfer/SendERC20.tsx
+++ src/app/transfer/SendERC20.tsx
@@ -5,7 +5,8 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { disconnect } from "@wagmi/core";
 import { tokens } from "../utils/chainAndTokens";
 import { useRouter } from "next/navigation";
-import CustomModal from "../components/CustomModal";
+import CustomModal, { modalOnError } from "../components/CustomModal";
+import WaitForTransaction from "./WaitForTransaction";
 
 export interface SendERC20Props {
   tokenName: string;
@@ -37,10 +38,10 @@ export default function SendERC20(props: SendERC20Props) {
   const router = useRouter();
   const [modal, setModal] = useState<any>();
   const handleCloseModal = () => {
-    disconnect().then(()=>{
+    disconnect().then(() => {
       setModal(undefined);
       router.back();
-    })
+    });
   };
 
   // prepare the transaction
@@ -56,20 +57,35 @@ export default function SendERC20(props: SendERC20Props) {
   });
 
   // get the transfer function
-  const { data, isLoading, isSuccess, isError, write } =
-    useContractWrite(config);
+  const { data, isLoading, isSuccess, isError, write } = useContractWrite({
+    ...config,
+    onError: (error) => {
+      const cause = `${error.cause}`;
+      const errMsg = "UserRejectedRequestError:";
+      modalOnError(
+        {
+          counter: 3,
+          name: `Go back after `,
+          message: cause.includes(errMsg)
+            ? "User rejected the request."
+            : error.cause,
+        },
+        setModal,
+        () => {
+          disconnect().then(router.back);
+        }
+      );
+    },
+  });
 
   useEffect(() => {
     setModal(undefined);
     console.log("useBalance: data:=:", dataBalance);
-    if (
-      (error || isErrorBalance) &&
-      Number(dataBalance?.formatted ?? 0) <= 0
-    ) {
-      setModal({
+    if ((error || isErrorBalance) && Number(dataBalance?.formatted ?? 0) <= 0) {
+      modalOnError({
         name: `Error Type: transferAmountExceedsBalance`,
         message: "You don't have enough tokens for this transaction.",
-      });
+      }, setModal);
     } else if (props.callFunction === "call" && write) {
       console.log("sending erc20:=: call:", props?.callFunction);
       write?.();
@@ -77,22 +93,18 @@ export default function SendERC20(props: SendERC20Props) {
     }
   }, [props.callFunction, write, dataBalance]);
 
-  useEffect(() => {
-    if (isSuccess || isError) {
-      console.log("isSuccess, isError :=:", isSuccess, isError);
-      console.log("tx data:=:", JSON.stringify(data));
-      (async () => {
-        await disconnect();
-        router.back();
-      })();
-    }
-  }, [isSuccess, isError]);
-
   return (
-    <CustomModal isOpen={modal} onClose={handleCloseModal}>
-      <h2>{modal?.name}</h2>
-      <p style={{ marginTop: 20 }}>{modal?.message}</p>
-    </CustomModal>
+    <>
+      {data?.hash && (
+        <WaitForTransaction txh={data.hash} chainId={props.chainId} />
+      )}
+      <CustomModal isOpen={modal} onClose={handleCloseModal}>
+      <h2>{`${
+          modal?.counter ? modal?.name + modal?.counter : modal?.name
+        }`}</h2>
+        <p style={{ marginTop: 20 }}>{modal?.message}</p>
+      </CustomModal>
+    </>
     // <div>
     //   <button disabled={!write} onClick={() => write?.()}>
     //     Transfer
